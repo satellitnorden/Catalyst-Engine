d9201561ffd56064896bc2d646b0d8e77971b54f
Daybreak <DennisMartenssons@gmail.com>
2018-01-07 18:43:46 +0100

    Re-added missing files and changed Main.cpp back to it's original state.

--- Engine Source/Engine Core/Source/Main.cpp
+++ Engine Source/Engine Core/Source/Main.cpp
@@ -1,4 +1,3 @@
-/*
 //Engine core.
 #include <EngineCore.h>
 
@@ -17,57 +16,5 @@ int main()
 	EngineSystem::Instance->ReleaseSystem();
 
 	//Return success!
-	return 0;
-}
-*/
-
-//Engine core.
-#include <EngineCore.h>
-
-//Math.
-#include <GameMath.h>
-#include <Vector3.h>
-
-//Tools.
-#include <MicroBenchmarker.h>
-
-#define MAP_ITERATIONS 1'000
-
-void Function1()
-{
-	Map<float, std::string> myMap;
-
-	for (uint16 i = 0; i < MAP_ITERATIONS; ++i)
-	{
-		myMap.Emplace(GameMath::RandomFloatInRange(0.0f, 100.0f), "Hjalmar");
-	}
-
-	for (uint16 i = 0; i < MAP_ITERATIONS; ++i)
-	{
-		volatile std::string *CATALYST_RESTRICT myString = myMap.Find(GameMath::RandomFloatInRange(0.0f, 100.0f));
-	}
-}
-
-void Function2()
-{
-	std::map<float, std::string> myMap;
-
-	for (uint16 i = 0; i < MAP_ITERATIONS; ++i)
-	{
-		myMap.emplace(GameMath::RandomFloatInRange(0.0f, 100.0f), "Hjalmar");
-	}
-
-	for (uint16 i = 0; i < MAP_ITERATIONS; ++i)
-	{
-		volatile auto myString = myMap.find(GameMath::RandomFloatInRange(0.0f, 100.0f));
-	}
-}
-
-int main()
-{
-	MicroBenchmarker::StartBenchmark(100, &Function1, &Function2);
-
-	std::cin.ignore();
-
 	return 0;
 }
