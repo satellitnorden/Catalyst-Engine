f34d802deab27ffeee318d4707f106b8c1b1db37
Daybreak <DennisMartenssons@gmail.com>
2018-05-03 14:37:59 +0200

    Fixed benchmark macros

--- Engine/Engine Source/Include/Engine Core/CoreMacros.h
+++ Engine/Engine Source/Include/Engine Core/CoreMacros.h
@@ -2,42 +2,28 @@
 
 /*
 *	Tracks the average execution time of a given function or section of code and prints the average execution time in non-final builds.
-*/
-#if !defined(CATALYST_FINAL)
-	#define CATALYST_BENCHMARK_SECTION_AVERAGE(function)																										\
-	{																																							\
-		static DynamicArray<long long, 1'000'000> durations;																									\
-		static long long averageDuration = 0;																													\
-		auto timeBeforeFunction = std::chrono::high_resolution_clock::now();																					\
-		function;																																				\
-		durations.EmplaceSlow(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - timeBeforeFunction).count());	\
-		averageDuration += durations.Back();																													\
-		float duration = StaticCast<float>(averageDuration / durations.Size()) / 1'000.0f;																		\
-		PRINT_TO_CONSOLE(#function << " - " << duration << " milliseconds.");																					\
-	}
-#else
-	#define CATALYST_BENCHMARK_SECTION_AVERAGE(function) function;
-#endif
-
-/*
-*	Tracks the average execution time of a given function or section of code and prints the average execution time in non-final builds.
 *	Allows for a personalized message.
 */
 #if !defined(CATALYST_FINAL)
 	#define CATALYST_BENCHMARK_NAMED_SECTION_AVERAGE(message, function)																							\
 	{																																							\
-		static DynamicArray<long long, 1'000'000> durations;																									\
-		static long long averageDuration = 0;																													\
+		static uint64 iterations = 0;																															\
+		static uint64 averageDuration = 0;																														\
 		auto timeBeforeFunction = std::chrono::high_resolution_clock::now();																					\
 		function;																																				\
-		durations.EmplaceSlow(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - timeBeforeFunction).count());	\
-		averageDuration += durations.Back();																													\
-		float duration = static_cast<float>(averageDuration / durations.Size()) / 1'000.0f;																		\
+		averageDuration += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - timeBeforeFunction).count(); 		\
+		++iterations;																																			\
+		float duration = static_cast<float>(averageDuration / iterations) / 1'000.0f;																			\
 		PRINT_TO_CONSOLE(message << " - " << duration << " milliseconds.");																						\
 	}
 #else
 	#define CATALYST_BENCHMARK_NAMED_SECTION_AVERAGE(message, function) function;
 #endif
+
+/*
+*	Tracks the average execution time of a given function or section of code and prints the average execution time in non-final builds.
+*/
+#define CATALYST_BENCHMARK_SECTION_AVERAGE(function) CATALYST_BENCHMARK_NAMED_SECTION_AVERAGE(#function, function);
 
 /*
 *	Declares a singleton class. Must be done inside the class in the header file.
