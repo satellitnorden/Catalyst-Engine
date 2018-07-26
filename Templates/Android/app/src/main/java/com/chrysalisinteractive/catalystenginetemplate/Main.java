69e09e7ae13c24b1e26ab8f0f0ed6d253c3da7f8
Daybreak <DennisMartenssons@gmail.com>
2018-07-26 17:44:44 +0200

    Got stuff building at least

--- Templates/Android/app/src/main/java/com/chrysalisinteractive/catalystenginetemplate/Main.java
+++ Templates/Android/app/src/main/java/com/chrysalisinteractive/catalystenginetemplate/Main.java
@@ -4,26 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Main extends AppCompatActivity {
+public class Main extends AppCompatActivity
+{
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+    static
+    {
+        //Load the necessary native libraries.
+        System.loadLibrary("catalyst-engine-lib");
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //Call the C++ main function.
+        main();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    /*
+    *   The C++ main function.
+    */
+    public native int main();
+
 }
