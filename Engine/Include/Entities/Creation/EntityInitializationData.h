ce7acc685f4790047f9166096aaaa4d32e18f614
Daybreak <DennisMartenssons@gmail.com>
2019-01-27 17:49:39 +0100

    Space (:

--- Engine/Include/Entities/Creation/EntityInitializationData.h
+++ Engine/Include/Entities/Creation/EntityInitializationData.h
@@ -22,6 +22,7 @@ public:
 		*	will be destroyed as soon as the entity has been terminated.
 		*/
 		AutomaticDestruction = BIT(1),
+
 		/*
 		*	Entities initialized with the automatic termination property will be automatically terminated
 		*	at a point defined by it's entity type. Not all entity types support automatic termination.
