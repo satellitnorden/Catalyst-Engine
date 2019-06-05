73579d1f810c8a1c45aff781db733af663240976
Daybreak <DennisMartenssons@gmail.com>
2019-06-05 12:07:27 +0200

    tweak

--- Engine/Include/Input/InputCore.h
+++ Engine/Include/Input/InputCore.h
@@ -6,7 +6,7 @@
 //Input constants.
 namespace InputConstants
 {
-	constexpr float GAMEPAD_DEADZONE{ 0.155f }; //0.0025f step.
+	constexpr float GAMEPAD_DEADZONE{ 0.1575f }; //0.0025f step.
 	constexpr uint8 MAXIMUM_NUMBER_OF_GAMEPADS{ 4 };
 }
 
