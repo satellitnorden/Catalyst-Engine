4fc8f0a7b2cdb6adb84c5e6d0d7340255716e4be
Daybreak <DennisMartenssons@gmail.com>
2019-07-23 20:02:51 +0200

    Adding some form or support for raycasting against terrain (even though it's kinda bad and slow in it's current state)

--- Engine/Source/Systems/PhysicsSystem.cpp
+++ Engine/Source/Systems/PhysicsSystem.cpp
@@ -64,17 +64,31 @@ void PhysicsSystem::CastRay(const Ray &ray, const PhysicsChannel channels, Rayca
 */
 void PhysicsSystem::CastRayTerrain(const Ray &ray, RaycastResult *const RESTRICT result) NOEXCEPT
 {
-	float intersectionDistance;
+	//Ray march until the ray has hit the terrain.
+	const uint32 number_of_steps{ static_cast<uint32>(ray._MaximumHitDistance) + 1 };
 
-	if (CatalystGeometryMath::RayPlaneIntersection(ray,
-													Plane(VectorConstants::ZERO, VectorConstants::UP),
-													&intersectionDistance))
+	for (uint32 i{ 0 }; i < number_of_steps; ++i)
 	{
-		if (intersectionDistance < result->_HitDistance)
+		//Calculate the current ray position.
+		const Vector3<float> current_ray_position{ ray._Origin + ray._Direction * static_cast<float>(i) };
+
+		//Get the terrain height at the current ray position.
+		float terrain_height;
+		
+		if (!TerrainSystem::Instance->GetTerrainHeightAtPosition(current_ray_position, &terrain_height))
+		{
+			//The ray is outside the bounds of the terrain, abort.
+			return;
+		}
+
+		//If the current ray position's height is lower than the terrain height at this position, there is a hit.
+		if (current_ray_position._Y <= terrain_height)
 		{
 			result->_HasHit = true;
-			result->_HitDistance = intersectionDistance;
+			result->_HitDistance = static_cast<float>(i);
 			result->_HitEntity = nullptr;
+
+			return;
 		}
 	}
 }
