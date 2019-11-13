744bbfacc69665e36bff0bf5eb7950caaa03339c
Daybreak <DennisMartenssons@gmail.com>
2019-11-13 12:57:28 +0100

    Cleaning up atomic queue

--- Engine/Include/Multithreading/AtomicQueue.h
+++ Engine/Include/Multithreading/AtomicQueue.h
@@ -10,7 +10,7 @@
 *	This is to avoid reallocations when pushing/popping, so the atomic queue MUST be initialized with large enough storage to ensure that this never happens.
 */
 
-template <typename Type, uint64 SIZE>
+template <typename TYPE, uint64 SIZE>
 class AtomicQueue final
 {
 
@@ -19,53 +19,55 @@ public:
 	/*
 	*	Pushes a new value into the queue.
 	*/
-	void Push(Type newValue) NOEXCEPT
+	void Push(TYPE new_value) NOEXCEPT
 	{
-		uint64 oldWriteIndex;
-		uint64 newWriteIndex;
+		uint64 old_write_index;
+		uint64 new_write_index;
 
 		do
 		{
-			oldWriteIndex = _WriteIndex.load();
+			old_write_index = _WriteIndex.load();
 
-			newWriteIndex = oldWriteIndex < (SIZE - 1) ? oldWriteIndex + 1 : 0;
-		} while (!_WriteIndex.compare_exchange_weak(oldWriteIndex, newWriteIndex));
+			new_write_index = old_write_index < (SIZE - 1) ? old_write_index + 1 : 0;
+		} while (!_WriteIndex.compare_exchange_weak(old_write_index, new_write_index));
 
-		_Queue[oldWriteIndex] = newValue;
+		_Queue[old_write_index] = new_value;
 
-		uint64 expectedLastIndex{ newWriteIndex > 0 ? newWriteIndex - 1 : SIZE - 1 };
-		uint64 newLastIndex{ expectedLastIndex < (SIZE - 1) ? expectedLastIndex + 1 : 0 };
+		uint64 expected_last_index{ new_write_index > 0 ? new_write_index - 1 : SIZE - 1 };
+		uint64 new_last_index{ expected_last_index < (SIZE - 1) ? expected_last_index + 1 : 0 };
 
-		while (!_LastIndex.compare_exchange_weak(expectedLastIndex, newLastIndex));
+		while (!_LastIndex.compare_exchange_weak(expected_last_index, new_last_index));
 	}
 
 	/*
 	*	Pops a value from the queue, if the queue is not empty. If the pop was succesful, a pointer to the popped value is returned, otherwise nullptr.
 	*/
-	RESTRICTED Type *const RESTRICT Pop() NOEXCEPT
+	RESTRICTED TYPE*const RESTRICT Pop() NOEXCEPT
 	{
-		uint64 oldFirstIndex;
-		uint64 oldLastIndex;
-		uint64 newFirstIndex;
+		uint64 old_first_index;
+		uint64 old_last_index;
+		uint64 new_first_index;
 
 		do
 		{
-			oldFirstIndex = _FirstIndex.load();
-			oldLastIndex = _LastIndex.load();
+			old_first_index = _FirstIndex.load();
+			old_last_index = _LastIndex.load();
 
-			if (oldFirstIndex == oldLastIndex)
+			if (old_first_index == old_last_index)
+			{
 				return nullptr;
+			}
 
-			newFirstIndex = oldFirstIndex < (SIZE - 1) ? oldFirstIndex + 1 : 0;
-		} while (!_FirstIndex.compare_exchange_weak(oldFirstIndex, newFirstIndex));
+			new_first_index = old_first_index < (SIZE - 1) ? old_first_index + 1 : 0;
+		} while (!_FirstIndex.compare_exchange_weak(old_first_index, new_first_index));
 
-		return &_Queue[oldFirstIndex];
+		return &_Queue[old_first_index];
 	}
 
 private:
 
 	//The underlying queue.
-	StaticArray<Type, SIZE> _Queue;
+	StaticArray<TYPE, SIZE> _Queue;
 
 	//The first index in the queue.
 	std::atomic<uint64> _FirstIndex{ 0 };
