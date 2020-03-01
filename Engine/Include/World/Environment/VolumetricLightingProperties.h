e0f042abe0d6288546b38b5df339f93fb8819546
Daybreak <DennisMartenssons@gmail.com>
2020-03-01 17:27:05 +0100

    yes

--- Engine/Include/World/Environment/VolumetricLightingProperties.h
+++ Engine/Include/World/Environment/VolumetricLightingProperties.h
@@ -9,12 +9,12 @@ class VolumetricLightingProperties final
 public:
 
 	//The distance.
-	float _Distance{ 2'048.0f };
+	float32 _Distance{ 2'048.0f };
 
 	//The height.
-	float _Height{ 2'048.0f };
+	float32 _Height{ 1'024.0f };
 
 	//The thickness.
-	float _Thickness{ 1.0f };
+	float32 _Thickness{ 1.0f };
 
 };
