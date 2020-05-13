db7d8504ac3c6c969bfda52a98e0020e2717852c
Daybreak <DennisMartenssons@gmail.com>
2020-05-13 13:00:22 +0200

    worldtransform progress

--- Engine/Include/World/Core/WorldTransform.h
+++ Engine/Include/World/Core/WorldTransform.h
@@ -26,12 +26,133 @@ public:
 	*/
 	FORCE_INLINE constexpr WorldTransform() NOEXCEPT
 		:
-		_Cell(),
-		_Position(),
-		_Rotation(),
-		_Scale()
+		_Cell(0, 0, 0),
+		_LocalPosition(0.0f, 0.0f, 0.0f),
+		_Rotation(0.0f, 0.0f, 0.0f),
+		_Scale(1.0f)
 	{
 
+	}
+
+	/*
+	*	Constructor taking all values as arguments.
+	*/
+	FORCE_INLINE constexpr WorldTransform(	const Vector3<int32> &initial_cell,
+											const Vector3<float32> &initial_local_position,
+											const Vector3<float32> &initial_rotation,
+											const float32 initial_scale) NOEXCEPT
+		:
+		_Cell(initial_cell),
+		_LocalPosition(initial_local_position),
+		_Rotation(initial_rotation),
+		_Scale(initial_scale)
+	{
+		//Update the cell.
+		UpdateCell();
+	}
+
+	/*
+	*	Constructor taking the local position, rotation and scale.
+	*/
+	FORCE_INLINE constexpr WorldTransform(	const Vector3<float32> &initial_local_position,
+											const Vector3<float32> &initial_rotation,
+											const float32 initial_scale) NOEXCEPT
+		:
+		_Cell(0, 0, 0),
+		_LocalPosition(initial_local_position),
+		_Rotation(initial_rotation),
+		_Scale(initial_scale)
+	{
+		//Update the cell.
+		UpdateCell();
+	}
+
+	/*
+	*	Returns the cell.
+	*/
+	FORCE_INLINE NO_DISCARD const Vector3<int32> &GetCell() const NOEXCEPT
+	{
+		return _Cell;
+	}
+
+	/*
+	*	Sets the cell.
+	*/
+	FORCE_INLINE void SetCell(const Vector3<int32> &value) NOEXCEPT
+	{
+		_Cell = value;
+	}
+
+	/*
+	*	Returns the local position.
+	*/
+	FORCE_INLINE NO_DISCARD const Vector3<float32> &GetLocalPosition() const NOEXCEPT
+	{
+		return _LocalPosition;
+	}
+
+	/*
+	*	Sets the local position.
+	*/
+	FORCE_INLINE void SetLocalPosition(const Vector3<float32> &value) NOEXCEPT
+	{
+		_LocalPosition = value;
+
+		UpdateCell();
+	}
+
+	/*
+	*	Returns the absolute position.
+	*/
+	FORCE_INLINE NO_DISCARD const Vector3<float32> &GetAbsolutePosition() const NOEXCEPT
+	{	
+		const float32 world_grid_size{ WorldSystem::Instance->GetWorldGridSize() };
+
+		return _LocalPosition + Vector3<float32>(static_cast<float32>(_Cell._X), static_cast<float32>(_Cell._Y), static_cast<float32>(_Cell._Z)) * world_grid_size;
+	}
+
+	/*
+	*	Returns the relative position.
+	*/
+	FORCE_INLINE NO_DISCARD const Vector3<float32> &GetRelativePosition(const Vector3<int32> &cell) const NOEXCEPT
+	{	
+		const float32 world_grid_size{ WorldSystem::Instance->GetWorldGridSize() };
+
+		const Vector3<int32> delta{ _Cell - cell };
+
+		return _LocalPosition + Vector3<float32>(static_cast<float32>(delta._X), static_cast<float32>(delta._Y), static_cast<float32>(delta._Z)) * world_grid_size * world_grid_size;
+	}
+
+	/*
+	*	Returns the rotation.
+	*/
+	FORCE_INLINE NO_DISCARD const Vector3<float32> &GetRotation() const NOEXCEPT
+	{
+		return _Rotation;
+	}
+
+	/*
+	*	Sets the rotation.
+	*/
+	FORCE_INLINE void SetRotation(const Vector3<float32> &value) NOEXCEPT
+	{
+		_Rotation = value;
+	}
+
+	/*
+	*	Returns the scale.
+	*/
+	FORCE_INLINE NO_DISCARD float32 GetScale() const NOEXCEPT
+	{
+		return _Scale;
+	}
+
+	/*
+	*	Sets the scale.
+	*/
+	FORCE_INLINE void SetScale(const float32 value) NOEXCEPT
+	{
+		_Scale = value;
 	}
 
 	/*
@@ -39,7 +160,9 @@ public:
 	*/
 	FORCE_INLINE NO_DISCARD Matrix4x4 ToAbsoluteMatrix4x4() const NOEXCEPT
 	{
-		return Matrix4x4(_Position + Vector3<float32>(static_cast<float32>(_Cell._X), static_cast<float32>(_Cell._Y), static_cast<float32>(_Cell._Z)) * WorldSystem::Instance->GetWorldGridSize(), _Rotation, Vector3<float32>(_Scale));
+		const float32 world_grid_size{ WorldSystem::Instance->GetWorldGridSize() };
+
+		return Matrix4x4(_LocalPosition + Vector3<float32>(static_cast<float32>(_Cell._X), static_cast<float32>(_Cell._Y), static_cast<float32>(_Cell._Z)) * world_grid_size, _Rotation, Vector3<float32>(_Scale));
 	}
 
 	/*
@@ -47,9 +170,11 @@ public:
 	*/
 	FORCE_INLINE NO_DISCARD Matrix4x4 ToRelativeMatrix4x4(const Vector3<int32> &cell) const NOEXCEPT
 	{
+		const float32 world_grid_size{ WorldSystem::Instance->GetWorldGridSize() };
+
 		const Vector3<int32> delta{ _Cell - cell };
 
-		return Matrix4x4(_Position + Vector3<float32>(static_cast<float32>(delta._X), static_cast<float32>(delta._Y), static_cast<float32>(delta._Z)) * WorldSystem::Instance->GetWorldGridSize(), _Rotation, Vector3<float32>(_Scale));
+		return Matrix4x4(_LocalPosition + Vector3<float32>(static_cast<float32>(delta._X), static_cast<float32>(delta._Y), static_cast<float32>(delta._Z)) * world_grid_size, _Rotation, Vector3<float32>(_Scale));
 	}
 
 private:
@@ -57,8 +182,8 @@ private:
 	//The cell.
 	Vector3<int32> _Cell;
 
-	//The position.
-	Vector3<float32> _Position;
+	//The local position.
+	Vector3<float32> _LocalPosition;
 
 	//The rotation.
 	Vector3<float32> _Rotation;
@@ -74,39 +199,39 @@ private:
 		const float32 world_grid_size{ WorldSystem::Instance->GetWorldGridSize() };
 		const float32 half_world_grid_size{ world_grid_size * 0.5f };
 
-		while (_Position._X < -half_world_grid_size)
+		while (_LocalPosition._X < -half_world_grid_size)
 		{
-			_Position._X += world_grid_size;
+			_LocalPosition._X += world_grid_size;
 			--_Cell._X;
 		}
 
-		while (_Position._X > half_world_grid_size)
+		while (_LocalPosition._X > half_world_grid_size)
 		{
-			_Position._X -= world_grid_size;
+			_LocalPosition._X -= world_grid_size;
 			++_Cell._X;
 		}
 
-		while (_Position._Y < -half_world_grid_size)
+		while (_LocalPosition._Y < -half_world_grid_size)
 		{
-			_Position._Y += world_grid_size;
+			_LocalPosition._Y += world_grid_size;
 			--_Cell._Y;
 		}
 
-		while (_Position._Y > half_world_grid_size)
+		while (_LocalPosition._Y > half_world_grid_size)
 		{
-			_Position._Y -= world_grid_size;
+			_LocalPosition._Y -= world_grid_size;
 			++_Cell._Y;
 		}
 
-		while (_Position._Z < -half_world_grid_size)
+		while (_LocalPosition._Z < -half_world_grid_size)
 		{
-			_Position._Z += world_grid_size;
+			_LocalPosition._Z += world_grid_size;
 			--_Cell._Z;
 		}
 
-		while (_Position._Z > half_world_grid_size)
+		while (_LocalPosition._Z > half_world_grid_size)
 		{
-			_Position._Z -= world_grid_size;
+			_LocalPosition._Z -= world_grid_size;
 			++_Cell._Z;
 		}
 	}
