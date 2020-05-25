ff1ecba919dbad90b0533fa4b530f56e0d07517e
Daybreak <DennisMartenssons@gmail.com>
2020-05-25 19:25:07 +0200

    Adding some options for the selected entity in editor builds (:

--- Engine/Source/Editor/EditorSelectionSystem.cpp
+++ Engine/Source/Editor/EditorSelectionSystem.cpp
@@ -19,6 +19,9 @@
 #include <Systems/RenderingSystem.h>
 #include <Systems/ResourceSystem.h>
 
+//Third party.
+#include <ThirdParty/imgui.h>
+
 /*
 *	Initializes the editor selection system.
 */
@@ -46,84 +49,87 @@ void EditorSelectionSystem::PhysicsUpdate() NOEXCEPT
 		return;
 	}
 
-	//Cache if the left mouse button is pressed.
-	const bool left_mouse_button_pressed{ InputSystem::Instance->GetMouseState()->_Left == ButtonState::PRESSED };
-
 	//Cast a ray from the mouse position and forward.
-	Ray ray;
-
-	ray.SetOrigin(Perceiver::Instance->GetWorldTransform().GetAbsolutePosition());
-	ray.SetDirection(RenderingUtilities::CalculateRayDirectionFromScreenCoordinate(Vector2<float32>(InputSystem::Instance->GetMouseState()->_CurrentX, InputSystem::Instance->GetMouseState()->_CurrentY)));
-
-	RaycastConfiguration configuration;
-
-	configuration._PhysicsChannels = PhysicsChannel::DYNAMIC_MODELS;
-	configuration._MaximumHitDistance = CatalystEngineSystem::Instance->GetProjectConfiguration()->_RenderingConfiguration._ViewDistance;
-	configuration._TerrainRayMarchStep = 1.0f;
-
-	RaycastResult result;
-
-	PhysicsSystem::Instance->CastRay(ray, configuration, &result);
-
-	if (result._HasHit)
+	if (!ImGui::IsAnyItemHovered())
 	{
-		switch (result._Type)
+		//Cache if the left mouse button is pressed.
+		const bool left_mouse_button_pressed{ InputSystem::Instance->GetMouseState()->_Left == ButtonState::PRESSED };
+
+		Ray ray;
+
+		ray.SetOrigin(Perceiver::Instance->GetWorldTransform().GetAbsolutePosition());
+		ray.SetDirection(RenderingUtilities::CalculateRayDirectionFromScreenCoordinate(Vector2<float32>(InputSystem::Instance->GetMouseState()->_CurrentX, InputSystem::Instance->GetMouseState()->_CurrentY)));
+
+		RaycastConfiguration configuration;
+
+		configuration._PhysicsChannels = PhysicsChannel::DYNAMIC_MODELS;
+		configuration._MaximumHitDistance = CatalystEngineSystem::Instance->GetProjectConfiguration()->_RenderingConfiguration._ViewDistance;
+		configuration._TerrainRayMarchStep = 1.0f;
+
+		RaycastResult result;
+
+		PhysicsSystem::Instance->CastRay(ray, configuration, &result);
+
+		if (result._HasHit)
 		{
-			case RaycastResult::Type::DYNAMIC_MODEL:
+			switch (result._Type)
 			{
-				if (left_mouse_button_pressed)
+				case RaycastResult::Type::DYNAMIC_MODEL:
 				{
-					_CurrentlySelectedEntity = result._DynamicModelRaycastResult._Entity;
+					if (left_mouse_button_pressed)
+					{
+						_CurrentlySelectedEntity = result._DynamicModelRaycastResult._Entity;
+					}
+
+					else
+					{
+						_CurrentlyHoveredEntity = result._DynamicModelRaycastResult._Entity;
+					}
+
+					break;
 				}
 
-				else
+				case RaycastResult::Type::STATIC_MODEL:
 				{
-					_CurrentlyHoveredEntity = result._DynamicModelRaycastResult._Entity;
+					if (left_mouse_button_pressed)
+					{
+						_CurrentlySelectedEntity = result._StaticModelRaycastResult._Entity;
+					}
+
+					else
+					{
+						_CurrentlyHoveredEntity = result._StaticModelRaycastResult._Entity;
+					}
+
+					break;
 				}
 
-				break;
-			}
-
-			case RaycastResult::Type::STATIC_MODEL:
-			{
-				if (left_mouse_button_pressed)
+				case RaycastResult::Type::TERRAIN:
 				{
-					_CurrentlySelectedEntity = result._StaticModelRaycastResult._Entity;
+
+
+					break;
 				}
 
-				else
+				default:
 				{
-					_CurrentlyHoveredEntity = result._StaticModelRaycastResult._Entity;
+					ASSERT(false, "Invalid case!");
+
+					break;
 				}
-
-				break;
-			}
-
-			case RaycastResult::Type::TERRAIN:
-			{
-
-
-				break;
-			}
-
-			default:
-			{
-				ASSERT(false, "Invalid case!");
-
-				break;
 			}
 		}
-	}
 
-	else
-	{
-		//Reset the currently hovered entity.
-		_CurrentlyHoveredEntity = nullptr;
-
-		//If the left mouse button was pressed without any hit, reset the currently selected entity.
-		if (left_mouse_button_pressed)
+		else
 		{
-			_CurrentlySelectedEntity = nullptr;
+			//Reset the currently hovered entity.
+			_CurrentlyHoveredEntity = nullptr;
+
+			//If the left mouse button was pressed without any hit, reset the currently selected entity.
+			if (left_mouse_button_pressed)
+			{
+				_CurrentlySelectedEntity = nullptr;
+			}
 		}
 	}
 
@@ -147,9 +153,9 @@ void EditorSelectionSystem::PhysicsUpdate() NOEXCEPT
 		}
 	}
 
-	//Render a bounding box for the currently selected entity.
 	if (_CurrentlySelectedEntity && _CurrentlySelectedEntity->_Initialized)
 	{
+		//Render a bounding box for the currently selected entity.
 		switch (_CurrentlySelectedEntity->_Type)
 		{
 			case EntityType::DynamicModel:
@@ -165,6 +171,51 @@ void EditorSelectionSystem::PhysicsUpdate() NOEXCEPT
 				break;
 			}
 		}
+
+		//Display a screen with this entities properties.
+		ImGui::Begin("Selected Entity", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings);
+		ImGui::SetWindowPos(ImVec2(1'920.0f - 8.0f - 256.0f, 8.0f));
+		ImGui::SetWindowSize(ImVec2(256.0f, 128.0f));
+		
+		switch (_CurrentlySelectedEntity->_Type)
+		{
+			case EntityType::DynamicModel:
+			{
+				//Add the header text.
+				ImGui::Text("Dynamic Model");
+
+				//Cache the world transform.
+				WorldTransform *const RESTRICT world_transform{ static_cast<DynamicModelEntity *const RESTRICT>(_CurrentlySelectedEntity)->ModifyWorldTransform() };
+
+				//Add the position editor.
+				Vector3<float32> position{ world_transform->GetAbsolutePosition() };
+
+				if (ImGui::DragFloat3("Position", reinterpret_cast<float32 *const RESTRICT>(&position), 0.01f))
+				{
+					world_transform->SetAbsolutePosition(position);
+				}
+
+				//Add the rotation editor.
+				Vector3<float32> rotation{ world_transform->GetRotation() };
+
+				rotation._X = CatalystBaseMath::RadiansToDegrees(rotation._X);
+				rotation._Y = CatalystBaseMath::RadiansToDegrees(rotation._Y);
+				rotation._Z = CatalystBaseMath::RadiansToDegrees(rotation._Z);
+
+				if (ImGui::DragFloat3("Rotation", reinterpret_cast<float32 *const RESTRICT>(&rotation), 0.01f))
+				{
+					rotation._X = CatalystBaseMath::DegreesToRadians(rotation._X);
+					rotation._Y = CatalystBaseMath::DegreesToRadians(rotation._Y);
+					rotation._Z = CatalystBaseMath::DegreesToRadians(rotation._Z);
+
+					world_transform->SetRotation(rotation);
+				}
+
+				break;
+			}
+		}
+
+		ImGui::End();
 	}
 }
 #endif
