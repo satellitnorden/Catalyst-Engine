a8302c65fdb343ab3a9abdcb315bbbd2cf3c9aff
Daybreak <DennisMartenssons@gmail.com>
2020-06-05 00:53:27 +0200

    creating model resources I guessszszszs

--- Engine/Include/Editor/EditorResourcesSystem.h
+++ Engine/Include/Editor/EditorResourcesSystem.h
@@ -3,6 +3,8 @@
 
 //Core.
 #include <Core/Essential/CatalystEssential.h>
+#include <Core/Containers/DynamicArray.h>
+#include <Core/General/DynamicString.h>
 
 class EditorResourcesSystem final
 {
@@ -14,6 +16,35 @@ public:
 	*/
 	void Update() NOEXCEPT;
 
-};
+private:
 
+	//Enumeration covering all create resource modes.
+	enum class CreateResourceMode : uint8
+	{
+		NONE,
+		MODEL
+	};
+
+	/*
+	*	Create model resource data class definition.
+	*/
+	class CreateModelResourceData final
+	{
+
+	public:
+
+		//The output file path.
+		DynamicString _OutputFilePath;
+
+		DynamicArray<DynamicString> _LevelOfDetailFilepaths;
+
+	};
+
+	//The current create resource mode.
+	CreateResourceMode _CurrentCreateResourceMode{ CreateResourceMode::NONE };
+
+	//The create model resource data.
+	CreateModelResourceData _CreateModelResourceData;
+
+};
 #endif
--- Engine/Source/Editor/EditorResourcesSystem.cpp
+++ Engine/Source/Editor/EditorResourcesSystem.cpp
@@ -2,6 +2,9 @@
 //Header file.
 #include <Editor/EditorResourcesSystem.h>
 
+//File.
+#include <File/Core/FileCore.h>
+
 //Systems.
 #include <Systems/CatalystEditorSystem.h>
 #include <Systems/ResourceSystem.h>
@@ -17,6 +20,8 @@ void EditorResourcesSystem::Update() NOEXCEPT
 	//Is the current contextual window RESOURCES?
 	if (CatalystEditorSystem::Instance->GetCurrentContextualWindow() != CatalystEditorSystem::ContextualWindow::RESOURCES)
 	{
+		_CurrentCreateResourceMode = CreateResourceMode::NONE;
+
 		return;
 	}
 
@@ -24,6 +29,116 @@ void EditorResourcesSystem::Update() NOEXCEPT
 	ImGui::Begin("Resources", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings);
 	ImGui::SetWindowPos(ImVec2(8.0f, 8.0f + 256.0f + 8.0f));
 	ImGui::SetWindowSize(ImVec2(256.0f, 512.0f));
+
+	//Add the button for creating a model resource.
+	if (ImGui::Button("Create Model Resource"))
+	{
+		_CurrentCreateResourceMode = CreateResourceMode::MODEL;
+	}
+
+	switch (_CurrentCreateResourceMode)
+	{
+		case CreateResourceMode::NONE:
+		{
+			//HWEL. Nothing to do here.
+
+			break;
+		}
+
+		case CreateResourceMode::MODEL:
+		{
+			//Add the entities window.
+			ImGui::Begin("Create Model Resource", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings);
+			ImGui::SetWindowPos(ImVec2(8.0f + 256.0f + 8.0f, 8.0f + 256.0f + 8.0f));
+			ImGui::SetWindowSize(ImVec2(512.0f, 512.0f));
+
+			//If the user has already selected a file path, display it.
+			if (_CreateModelResourceData._OutputFilePath.Data())
+			{
+				ImGui::Text("File Path:");
+				ImGui::Text(_CreateModelResourceData._OutputFilePath.Data());
+			}
+
+			//Add the button to set the output file path/identifier.
+			if (ImGui::Button("Select File Path"))
+			{
+				File::BrowseForFile(false, &_CreateModelResourceData._OutputFilePath);
+			}
+
+			//If the user has already added level of details, display them.
+			uint64 level_of_detail_counter{ 0 };
+
+			for (uint64 i{ 0 }, size{ _CreateModelResourceData._LevelOfDetailFilepaths.Size() }; i < size; ++i)
+			{
+				ImGui::Text("Level Of Detail #%llu File Path:", level_of_detail_counter + 1);
+				ImGui::Text(_CreateModelResourceData._LevelOfDetailFilepaths[i].Data());
+
+				++level_of_detail_counter;
+			}
+
+			//Add the button for adding level of detail file paths.
+			if (ImGui::Button("Add Level Of Detail"))
+			{
+				DynamicString chosen_file;
+
+				if (File::BrowseForFile(false, &chosen_file))
+				{
+					_CreateModelResourceData._LevelOfDetailFilepaths.Emplace(chosen_file);
+				}
+			}
+
+			//Add the create button.
+			if (ImGui::Button("Create"))
+			{
+				//Retrieve the identifier.
+				DynamicString identifier;
+
+				for (int64 i{ static_cast<int64>(_CreateModelResourceData._OutputFilePath.Length()) - 1 }; i >= 0; --i)
+				{
+					if (_CreateModelResourceData._OutputFilePath[i] == '\\')
+					{
+						identifier = &_CreateModelResourceData._OutputFilePath[i + 1];
+
+						break;
+					}
+				}
+
+				//Build the model.
+				{
+					ModelBuildParameters parameters;
+
+					parameters._Output = _CreateModelResourceData._OutputFilePath.Data();
+					parameters._ID = identifier.Data();
+
+					for (const DynamicString &level_of_detail_file_path : _CreateModelResourceData._LevelOfDetailFilepaths)
+					{
+						parameters._LevelOfDetails.Emplace(level_of_detail_file_path.Data());
+					}
+
+					parameters._Transformation = Matrix4x4(VectorConstants::ZERO, Vector3<float32>(-CatalystBaseMathConstants::HALF_PI, 0.0f, 0.0f), VectorConstants::ONE);
+					parameters._TextureCoordinateMultiplier = 1.0f;
+					parameters._TexturCoordinateRotation = 0.0f;
+
+					ResourceSystem::Instance->GetResourceBuildingSystem()->BuildModel(parameters);
+				}
+
+				//Now load the model.
+				_CreateModelResourceData._OutputFilePath += ".cr";
+				ResourceSystem::Instance->LoadResource(_CreateModelResourceData._OutputFilePath.Data());
+			}
+
+			ImGui::End();
+
+			break;
+		}
+
+		default:
+		{
+			ASSERT(false, "Invalid case!");
+
+			break;
+		}
+	}
 
 	ImGui::End();
 }
