d54057602b278474e49e44336056698af0e3a04d
Daybreak <DennisMartenssons@gmail.com>
2020-07-12 21:52:56 +0100

    Adding a null distribution system, so builds can be made without any particular distribution at all (:

---
+++ Engine/Source/Distribution/NullDistributionSystem.cpp
@@ -0,0 +1,44 @@
+#if !defined(CATALYST_DISTRIBUTION_STEAM)
+//Header file.
+#include <Systems/DistributionSystem.h>
+
+/*
+*	Initializes the distribution system.
+*/
+void DistributionSystem::Initialize() NOEXCEPT
+{
+	
+}
+
+/*
+*	Terminates the distribution system.
+*/
+void DistributionSystem::Terminate() NOEXCEPT
+{
+	
+}
+
+/*
+*	Returns the current user name.
+*/
+RESTRICTED NO_DISCARD const char *const RESTRICT DistributionSystem::CurrentUserName() const NOEXCEPT
+{
+	return nullptr;
+}
+
+/*
+*	Clears an achievement, resetting it's state.
+*/
+void DistributionSystem::ClearAchievement(const AchievementIdentifier &identifier) const NOEXCEPT
+{
+	
+}
+
+/*
+*	Grants an achievement.
+*/
+void DistributionSystem::GrantAchievement(const AchievementIdentifier &identifier) const NOEXCEPT
+{
+	
+}
+#endif
