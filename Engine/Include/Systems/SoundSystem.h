2499ce09f2507986604c0d8a9b1ddc0ec2b3523f
Daybreak <DennisMartenssons@gmail.com>
2020-09-04 23:25:29 +0200

    Tweaking NUMBER_OF_SAMPLES_PER_MIXING_BUFFER \o/

--- Engine/Include/Systems/SoundSystem.h
+++ Engine/Include/Systems/SoundSystem.h
@@ -23,7 +23,7 @@ public:
 	static constexpr uint8 NUMBER_OF_MIXING_BUFFERS{ 8 };
 
 	//The number of samples in each mixing buffer.
-	static constexpr uint32 NUMBER_OF_SAMPLES_PER_MIXING_BUFFER{ 128 };
+	static constexpr uint32 NUMBER_OF_SAMPLES_PER_MIXING_BUFFER{ 256 };
 
 	//Declare the singleton.
 	DECLARE_SINGLETON(SoundSystem);
