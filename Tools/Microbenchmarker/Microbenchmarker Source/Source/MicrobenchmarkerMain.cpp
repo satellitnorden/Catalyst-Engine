49608ced2f308ee239f7789e68805ecd603d0027
Daybreak <DennisMartenssons@gmail.com>
2021-01-20 23:45:04 +0100

    updating microbenchmarker

--- Tools/Microbenchmarker/Microbenchmarker Source/Source/MicrobenchmarkerMain.cpp
+++ Tools/Microbenchmarker/Microbenchmarker Source/Source/MicrobenchmarkerMain.cpp
@@ -8,6 +8,12 @@
 //Microbenchmarker.
 #include <Microbenchmarker.h>
 
+#define TEST_HASH_TABLE (0)
+#define TEST_PRE_VS_POST_INCREMENT (1)
+
+#define CURRENT_TEST TEST_PRE_VS_POST_INCREMENT
+
+#if CURRENT_TEST == TEST_HASH_TABLE
 //STD.
 #include <unordered_map>
 
@@ -47,3 +53,56 @@ int main() NOEXCEPT
 
 	return EXIT_SUCCESS;
 }
+#elif CURRENT_TEST == TEST_PRE_VS_POST_INCREMENT
+
+#define ELEMENTS (1'024 * 64)
+
+DynamicArray<uint64> first_array;
+DynamicArray<uint64> second_array;
+
+void FillArrays() NOEXCEPT
+{
+	for (uint64 i{ 0 }; i < ELEMENTS + 1; ++i)
+	{
+		first_array.Emplace(CatalystRandomMath::RandomIntegerInRange<uint64>(0, UINT64_MAXIMUM));
+	}
+
+	for (uint64 i{ 0 }; i < ELEMENTS + 1; ++i)
+	{
+		second_array.Emplace(CatalystRandomMath::RandomIntegerInRange<uint64>(0, UINT64_MAXIMUM));
+	}
+}
+
+void Function1() NOEXCEPT
+{
+	uint64 sum{ 0 };
+
+	for (uint64 i{ 0 }; i < ELEMENTS;)
+	{
+		sum += first_array[++i];
+	}
+
+	((volatile uint64*)sum)[0] = sum;
+}
+
+void Function2() NOEXCEPT
+{
+	uint64 sum{ 0 };
+
+	for (uint64 i{ 0 }; i < ELEMENTS;)
+	{
+		sum += second_array[i++];
+	}
+
+	((volatile uint64*)sum)[0] = sum;
+}
+
+int main() NOEXCEPT
+{
+	FillArrays();
+
+	Microbenchmarker::StartBenchmark(ELEMENTS, Function1, Function2);
+
+	return EXIT_SUCCESS;
+}
+#endif
