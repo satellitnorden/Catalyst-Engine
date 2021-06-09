57c3d58b35a14be7a49f1098d69b4fc3028300be
Daybreak <DennisMartenssons@gmail.com>
2021-06-10 00:16:09 +0200

    new editor colours \o/

--- Engine/Source/Systems/CatalystEditorSystem.cpp
+++ Engine/Source/Systems/CatalystEditorSystem.cpp
@@ -49,16 +49,29 @@ void CatalystEditorSystem::Initialize() NOEXCEPT
 	style->ScrollbarRounding = 0.0f;
 	style->GrabRounding = 0.0f;
 	style->TabRounding = 0.0f;
-	style->Colors[ImGuiCol_Text]					= ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-	style->Colors[ImGuiCol_WindowBg]				= PALETTE_BACKGROUND_COLOR;
-	style->Colors[ImGuiCol_Border]					= PALETTE_BORDER_COLOR;
-	style->Colors[ImGuiCol_BorderShadow]			= PALETTE_BACKGROUND_COLOR;
-	style->Colors[ImGuiCol_TitleBg]					= PALETTE_MAIN_COLOR_MEDIUM;
-	style->Colors[ImGuiCol_TitleBgActive]			= PALETTE_MAIN_COLOR_HIGH;
-	style->Colors[ImGuiCol_TitleBgCollapsed]		= PALETTE_MAIN_COLOR_LOW;
-	style->Colors[ImGuiCol_Button]					= PALETTE_MAIN_COLOR_LOW;
-	style->Colors[ImGuiCol_ButtonHovered]			= PALETTE_MAIN_COLOR_MEDIUM;
-	style->Colors[ImGuiCol_ButtonActive]			= PALETTE_MAIN_COLOR_HIGH;
+
+	{
+		auto &colors{ ImGui::GetStyle().Colors };
+
+		colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+		colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
+		colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
+		colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
+		colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+		colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+	}
 
 	//Register the update.
 	CatalystEngineSystem::Instance->RegisterUpdate([](void* const RESTRICT arguments)
