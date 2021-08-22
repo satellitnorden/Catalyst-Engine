92a9158b5e8c7a6307fdd77c9dc28bab5bf20734
Daybreak <DennisMartenssons@gmail.com>
2021-08-22 22:45:11 +0200

    bug fix

--- Engine/Source/ThirdParty/midifile/MidiMessage.cpp
+++ Engine/Source/ThirdParty/midifile/MidiMessage.cpp
@@ -1709,7 +1709,7 @@ void MidiMessage::makeNoteOff(int channel, int key, int velocity) {
 
 void MidiMessage::makeNoteOff(int channel, int key) {
 	resize(3);
-	(*this)[0] = 0x90 | (0x0f & channel);
+	(*this)[0] = 0x80 | (0x0f & channel);
 	(*this)[1] = key & 0x7f;
 	(*this)[2] = 0x00;
 }
