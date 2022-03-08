0b96409988173f48a28e2efd0e30fde9ce68ac85
Daybreak <DennisMartenssons@gmail.com>
2022-03-08 14:30:42 +0100

    oops

--- Engine/Source/Rendering/Native/Pipelines/GraphicsPipelines/UserInterfaceGraphicsPipeline.cpp
+++ Engine/Source/Rendering/Native/Pipelines/GraphicsPipelines/UserInterfaceGraphicsPipeline.cpp
@@ -108,7 +108,6 @@ void UserInterfaceGraphicsPipeline::Initialize() NOEXCEPT
 	SetStencilWriteMask(0);
 	SetStencilReferenceMask(0);
 	SetTopology(Topology::TriangleFan);
-	SetTopology(Topology::TriangleList);
 
 #if !defined(CATALYST_CONFIGURATION_FINAL)
 	//Set the name.
