46f9a4acbeabe2bd90835b7bc514ee4e59db0a89
Daybreak <DennisMartenssons@gmail.com>
2022-08-15 13:01:57 +0200

    Changing the default font. (:

--- Engine/Source/Resources/Building/CatalystEngineResourceBuilding.cpp
+++ Engine/Source/Resources/Building/CatalystEngineResourceBuilding.cpp
@@ -68,7 +68,7 @@ void CatalystEngineResourceBuilding::BuildResources() NOEXCEPT
 
 		parameters._Output = "..\\..\\..\\..\\Catalyst-Engine\\Engine\\Resources\\Intermediate\\Base\\Base\\Catalyst_Engine_Default_Font";
 		parameters._ID = "Catalyst_Engine_Default_Font";
-		parameters._File = "..\\..\\..\\..\\Catalyst-Engine\\Engine\\Resources\\Raw\\Fonts\\OpenSans-Regular.ttf";
+		parameters._File = "..\\..\\..\\..\\Catalyst-Engine\\Engine\\Resources\\Raw\\Fonts\\Kiona-Regular.ttf";
 
 		ResourceSystem::Instance->GetResourceBuildingSystem()->BuildFont(parameters);
 	}
