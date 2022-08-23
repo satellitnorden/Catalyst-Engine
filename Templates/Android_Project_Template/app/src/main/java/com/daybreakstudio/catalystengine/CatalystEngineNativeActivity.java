5f7f91f56d3b51c28c3735e5cbf9a1a45c4d1262
Daybreak <DennisMartenssons@gmail.com>
2022-08-23 13:10:00 +0200

    setting up custom NativeActivity for Android projects. (:

---
+++ Templates/Android_Project_Template/app/src/main/java/com/daybreakstudio/catalystengine/CatalystEngineNativeActivity.java
@@ -0,0 +1,10 @@
+//Package declaration.
+package com.daybreakstudio.catalystengine;
+
+//Imports.
+import android.app.NativeActivity;
+
+public class CatalystEngineNativeActivity extends NativeActivity
+{
+
+}
