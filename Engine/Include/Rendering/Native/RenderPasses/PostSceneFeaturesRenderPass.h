9343796a3384b35283745f942a36c10ef9883f4a
Daybreak <DennisMartenssons@gmail.com>
2025-04-06 09:12:45 +0200

    Removing some unused stuff. (:

--- Engine/Include/Rendering/Native/RenderPasses/PostSceneFeaturesRenderPass.h
+++ Engine/Include/Rendering/Native/RenderPasses/PostSceneFeaturesRenderPass.h
@@ -6,7 +6,6 @@
 
 //Rendering.
 #include <Rendering/Native/Pipelines/Core/GraphicsRenderPipeline.h>
-#include <Rendering/Native/Pipelines/GraphicsPipelines/DepthDownsampleGraphicsPipeline.h>
 #include <Rendering/Native/RenderPasses/RenderPass.h>
 
 //Systems.
@@ -16,9 +15,6 @@ class PostSceneFeaturesRenderPass final : public RenderPass
 {
 	
 public:
-
-	//Constants.
-	constexpr static uint64 DEPTH_MIP_CHAIN_DEPTH{ 8 };
 
 	//System declaration.
 	CATALYST_SYSTEM
@@ -31,21 +27,7 @@ public:
 	*/
 	PostSceneFeaturesRenderPass() NOEXCEPT;
 
-	/*
-	*	Returns the depth chain render target at the given mip index.
-	*/
-	FORCE_INLINE NO_DISCARD RenderTargetHandle GetDepthMip(const uint8 mip_index) NOEXCEPT
-	{
-		return _DepthMipChain[mip_index];
-	}
-
 private:
-
-	//The depth mip chain.
-	StaticArray<RenderTargetHandle, DEPTH_MIP_CHAIN_DEPTH> _DepthMipChain;
-
-	//The depth downsample graphics pipelines.
-	StaticArray<DepthDownsampleGraphicsPipeline, DEPTH_MIP_CHAIN_DEPTH> _DepthDownsampleGraphicsPipelines;
 
 	//The scene features downsample graphics pipeline.
 	GraphicsRenderPipeline _SceneFeaturesDownsampleGraphicsPipeline{ HashString("SceneFeaturesDownsample_RenderPipeline") };
